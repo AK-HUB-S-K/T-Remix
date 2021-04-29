78b201a94be1dd97cda4fdf8ff51a7c450e8923e
AK HUB <83388700+AK-HUB-S@users.noreply.github.com>
2021-04-29 12:38:26 +0530

    Update t-remix.sh

--- T-Remix-master/t-remix.sh
+++ T-Remix-master/t-remix.sh
@@ -1,8 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 ######################################
 #        PROJECT: T-Remix            #
-#        Author: ARIJ ARMAN          #
-#  email: mrxanonymous633@gmailcom   #
+#        Author: AJAY O S            #
 ######################################
 
 #Installing the following packages to install T-Remix
@@ -50,7 +49,7 @@ echo "
 echo " "
 echo " "
 echo " 
-       <─────────────── [ Re-Coded by =*•.¸♡ARIJ ARMAN♡¸.•* ] ───────────────>            
+       <─────────────── [ Re-Coded by =*•.¸♡AJAY O S ♡¸.•* ] ───────────────>            
                                                                " |lolcat
 
 
@@ -173,7 +172,7 @@ echo " "
 
 echo " "
 echo "
-        <─────────────── [Re-Coded by *•.¸♡ARIJ ARMAN♡¸.•* ] ───────────────>              
+        <─────────────── [Re-Coded by *•.¸♡AJAY O S ♡¸.•* ] ───────────────>              
                             " |lolcat
 
 echo " "
@@ -186,28 +185,14 @@ bash ashi.sh
 sleep 3.0
 clear
 tput clear
-tput cup 3 30
-echo -e "\e[44m Like \e[41m Subscribe \e[m"
-tput cup 6 30
-echo -e "\x1b[42m ARIJ ARMAN's TECH KNOW LINUX channel \x1b[m"
 tput cup 9 0
 echo -en "\e[93m what is your name : \e[m"
 read answer
 tput cup 12 22
 echo -e "\e[104m hello! Open a New session!! $answer! \e[m"
-tput cup 15 0
-echo -en "\e[93m Have you subscribe my channel for hacking tutorial (y/n)? \e[m "
-read answer
 
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-tput cup 17 21
-    echo -e "\e[42m Thankyou for subscribing my channel \e[m"
-else
-tput cup 17 23
-    echo -e "\x1b[41m Please subsrcibe my channel for hacking tutorial \x1b[m"
-fi
 tput cup 20 0
-figlet -f slant "ARIJ ARMAN's TECH KNOW LINUX" |lolcat
+figlet -f slant "WELCOME TO AK HUB TOOLS " |lolcat
 echo " "
 echo " "
 #done setting up
